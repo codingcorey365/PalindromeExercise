cada6eb947f6e7172dc33ed7647a7f58303c3bd8
Corey Schroeder <codingcorey365@gmail.com>
2024-02-23 18:30:28 -0600

    commit

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -6,26 +6,28 @@ namespace PalindromeExercise
     {
             // Fork and clone the following:
             // Palindrome Exercise
-
             // Make sure the PalindromeExerciseTests project file has PalindromeExercise added as a reference.
-
             // → Right click Dependencies in PalindromeExerciseTests to check and add a Project Reference if need be
-
+            //--------------------------------------------------------
+            
             // Create a class named WordSmith in your PalindromeExercise project file.This is where you will eventually create your IsAPalindrome() method
-
             // In your WordSmith class, you will create a method(IsAPalindrome) that will take a parameter of type string and return a value of type bool.
-            // If the given input is a palindrome – return true, else return false. You will finish this method   after you write your tests.
 
-            // Write you test method in the UnitTest1.cs file - located in the PalindromeExerciseTests project file
+            // If the given input is a palindrome – return true, else return false.
+            // You will finish this method after you write your tests.
+
+            // Write your test method in the UnitTest1.cs file
+            // - located in the PalindromeExerciseTests project file
 
             // In UnitTest1.cs, remove the Fact portion and replace it with:
-
             // [Theory]
             // [InlineData()] // ← you will need to put some test data and expected result here.  
 
             // For example [InlineData(“racecar”, true)]
             // Finish the test with Arrange, Act, Assert and make your IsAPalindrome method pass the tests
             // Once finished, save, commit, and push back to Github
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
-using System;
+﻿using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("john", false)]
+        [InlineData("jose", false)]
+        [InlineData("amoriss", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsPalindromeTest(string word, bool expected)
         {
-
+            //Arrange
+            WordSmith ws = new WordSmith();
+            
+            //Act
+            var actual = ws.IsAPalindrome(word);
+            
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
